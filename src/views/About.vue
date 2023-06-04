5cc7bc6b2ae7cca6b23f4000c6bcdd8186b72495
nobody <nobody@nowhere.mars>
2023-06-04 20:01:46 +0800

    format codes

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,10 @@
 import utils from '@/misc/utils.js'
 
 const t = utils.getTranslator()
-
 </script>
 
 <template>
-  <pre class="m-4 dark:text-neutral-100">
+    <pre class="m-4 dark:text-neutral-100">
 <b>WebUI v0.0.2 2023-05</b>
 <a href="https://github.com/vrnobody/WebUI">https://github.com/vrnobody/WebUI</a>
 
