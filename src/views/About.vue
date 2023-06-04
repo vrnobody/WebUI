e7f2dea499b1b916a7143809534beb1e881aa7d2
nobody <nobody@nowhere.mars>
2023-06-04 19:59:12 +0800

    translate about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,23 @@
-<script setup></script>
+<script setup>
+import utils from '@/misc/utils.js'
+
+const t = utils.getTranslator()
+
+</script>
 
 <template>
-    <pre class="m-4 dark:text-neutral-100">
+  <pre class="m-4 dark:text-neutral-100">
 <b>WebUI v0.0.2 2023-05</b>
 <a href="https://github.com/vrnobody/WebUI">https://github.com/vrnobody/WebUI</a>
 
-<b>Servers panel and Luna manager:</b>
-click "ON" label to stop
-click name to restart
-click tag to change settings
-drag & drop to rearrange order
+<b>{{ t('servPanelAndLunaManagerTitle') }}</b>
+{{ t('servPanelAndLunaManagerContent') }}
 
-<b>Luna editor keyboard shortcuts:</b>
-ctrl f  Show search box
-ctrl h  Show replace box
-ctrl l  Show goto-line box
-ctrl s  Quick save
-ctrl -  Move cursor backward
-ctrl +  Move cursor forward
-F5      Run script
-F6      Stop script
-F7      Abort script
-F8      Clear log
-F12     Goto to definition
+<b>{{ t('lunaEditorKeyboardShortcutsTitle') }}</b>
+{{ t('lunaEditorKeyboardShortcutsContent') }}
 
-<b>Credits:</b>
-&lt;waiting for you&gt;
+<b>{{ t('creditsTitle') }}</b>
+{{ t('creditsContent') }}
   </pre>
 </template>
 
